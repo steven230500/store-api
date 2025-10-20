cb29f271d65e398faff3c776ddfffbf4c9ecd034
steven230500 <spatino.gu@gmail.com>
2025-10-20 10:12:31 -0500

    fixed docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -e
-# Si usas TypeORM:
-# node dist/migrations/run.js || true
-# o si tienes script:
-# pnpm migrate || true
 
-node dist/main.js
+# Si tienes migraciones:
+# node dist/migrations/run.js || true
+
+node dist/src/main.js
